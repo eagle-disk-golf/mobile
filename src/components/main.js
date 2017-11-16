136bdad4ca5c454c37196684e6301af7cb649e2d
Topi Niiranen <niiranentopi@gmail.com>
2017-11-16 12:54:28 +0200

    cardview and react-native-easy-grid

--- src/components/main.js
+++ src/components/main.js
@@ -1,44 +1,88 @@
 import React, {Component} from 'react';
 import {Image, TouchableOpacity} from 'react-native';
 import {Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right} from 'native-base';
+import { Col, Row, Grid } from "react-native-easy-grid";
 export default class CardImageExample extends Component {
   render() {
     return (
       <Container>
         <Content>
-          <TouchableOpacity onPress={() => this.props.navigation.navigate('Tracking')}>
-            <Card>
-              <CardItem>
-                <Left>
-                  <Body>
-                    <Text>New Game</Text>
-                    <Text note>Start a new game</Text>
-                  </Body>
-                </Left>
-              </CardItem>
-              <CardItem cardBody>
-                <Image source={require('../res/images/newgame_card.png')} style={{height: 200, width: null, flex: 1}} />
-              </CardItem>
-            </Card>
-          </TouchableOpacity>
-
-          <TouchableOpacity onPress={() => this.props.navigation.navigate('Summary')}>
-            <Card>
-              <CardItem>
-                <Left>
-                  <Body>
-                    <Text>High scores</Text>
-                    <Text note>Local high scores</Text>
-                  </Body>
-                </Left>
-              </CardItem>
-              <CardItem cardBody>
-                <Image source={require('../res/images/highscores_card.png')} style={{height: 200, width: null, flex: 1}} />
-              </CardItem>
-            </Card>
-          </TouchableOpacity>
-        </Content>
-      </Container>
+          <Grid>
+            <Col>
+              <Row>
+                  <TouchableOpacity onPress={() => this.props.navigation.navigate('Tracking')}>
+                  <Card>
+                    <CardItem>
+                      <Left>
+                        <Body>
+                          <Text>New Game</Text>
+                          <Text note>Start a new game</Text>
+                        </Body>
+                      </Left>
+                    </CardItem>
+                    <CardItem cardBody>
+                      <Image source={require('../res/images/newgame_card.png')} style={{height: 200}} />
+                    </CardItem>
+                  </Card>
+                  </TouchableOpacity>
+              </Row>  
+              <Row>
+                  <TouchableOpacity onPress={() => this.props.navigation.navigate('Summary')}>
+                  <Card>
+                    <CardItem>
+                      <Left>
+                        <Body>
+                          <Text>High scores</Text>
+                          <Text note>Local high scores</Text>
+                        </Body>
+                      </Left>
+                    </CardItem>
+                    <CardItem cardBody>
+                      <Image source={require('../res/images/highscores_card.png')} style={{height: 200}} />
+                    </CardItem>
+                  </Card>
+                  </TouchableOpacity>
+              </Row>
+            </Col>
+            <Col>
+            <Row>
+                    <TouchableOpacity onPress={() => this.props.navigation.navigate('Summary')}>
+                    <Card>
+                      <CardItem>
+                        <Left>
+                          <Body>
+                            <Text>High scores</Text>
+                            <Text note>Local high scores</Text>
+                          </Body>
+                        </Left>
+                      </CardItem>
+                     <CardItem cardBody>
+                        <Image source={require('../res/images/highscores_card.png')} style={{height: 200}} />
+                      </CardItem>
+                    </Card>
+                    </TouchableOpacity>
+                </Row>
+              <Row>
+                  <TouchableOpacity onPress={() => this.props.navigation.navigate('Tracking')}>
+                    <Card>
+                      <CardItem>
+                        <Left>
+                          <Body>
+                            <Text>New Game</Text>
+                            <Text note>Start a new game</Text>
+                         </Body>
+                        </Left>
+                      </CardItem>
+                      <CardItem cardBody>
+                        <Image source={require('../res/images/newgame_card.png')} style={{height: 200}} />
+                      </CardItem>
+                   </Card>
+                    </TouchableOpacity>
+                </Row>
+              </Col>
+            </Grid>
+          </Content>
+        </Container>
     );
   }
 }
