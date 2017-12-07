f089272887f1e762fe610dff677a2c0059cb4f59
k2346 <K2346@LABRANET.JAMK.FI>
2017-12-07 15:37:02 +0200

    merge

--- src/components/header/header-right.js
+++ src/components/header/header-right.js
@@ -10,12 +10,14 @@ import React, {Component} from 'react';
 import {View} from 'react-native';
 import {Text, Container, Content, List, ListItem, Body, Right, Center, Icon} from 'native-base';
 import {Col, Row, Grid} from "react-native-easy-grid";
-
+//App colors
+import {COLORS} from '../../res/styles/constants';
 
 export default class HeaderRight extends Component {
   render() {
     return (
-<Icon name="settings" style={{color: '#fff'}} />
+<Icon name='settings' style={{color: COLORS.default}} />
+
     );
   }
 }
