9ee89cef2837d0f1052a284745d5f7ad1599914b
Riku Laajala <riku.laajala@gmail.com>
2017-12-14 14:10:49 +0200

    Transparent borderBottom for HeaderLeft item

--- src/components/header/header-left.js
+++ src/components/header/header-left.js
@@ -3,7 +3,8 @@
   File created: 30.11.2017
   Made by: Jenni
   History:
-  
+  12.12.2017 Riku: Transparent borderBottom for item
+
 */
 
 import React, {Component} from 'react';
@@ -14,28 +15,26 @@ import {Col, Row, Grid} from "react-native-easy-grid";
 export default class HeaderLeft extends Component {
   render() {
     return (
-      <Item>
+      <Item style={styles.container}>
         <Thumbnail style={styles.headerImage} source={require('../../res/images/newgame_card.png')} />
-           <Text style={styles.title}>Eagle{"\n"}Disc{"\n"}Golf</Text>
-    </Item>
+        <Text style={styles.title}>Eagle{"\n"}Disc{"\n"}Golf</Text>
+      </Item>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    borderBottomColor: 'transparent'
+  },
   header: {
     backgroundColor: 'pink',
   },
-    
-    headerImage: {
+  headerImage: {
     height: 60,
     width: 60,
-
-
   },
-    
   title: {
-
     fontSize: 20,
     fontWeight: 'bold',
     marginLeft: 20,
