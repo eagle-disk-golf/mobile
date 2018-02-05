38f1194a590c386988ddccf6df39677bac5f7289
Riku Laajala <riku.laajala@gmail.com>
2018-02-05 13:59:10 +0200

    transparent text backgrounds in home screen cards

--- src/components/main.js
+++ src/components/main.js
@@ -7,7 +7,7 @@ import {globalStyles} from '../res/styles';
 // Other way to import components/variables/etc
 // import styles from '../res/styles'
 
-    
+
 export default class MainButton extends Component {
   render() {
   const resizeMode = 'stretch';
@@ -27,9 +27,9 @@ export default class MainButton extends Component {
             <TouchableOpacity style={[style.touch]} onPress={() => this.props.navigation.navigate('Summary')}>
                     <ImageBackground source={require('../res/images/summary_card.png')} style={[style.image]} resizeMode='stretch' />
               <Text style={[globalStyles.textDark, style.text]}>SUMMARY</Text>
-            </TouchableOpacity>  
+            </TouchableOpacity>
         </Card>
-        </Row> 
+        </Row>
       </Grid>
     );
   }
@@ -39,27 +39,28 @@ const style = StyleSheet.create({
   touch: {
     flex: 1,
   },
-    card: {
-        marginLeft:5,
-        marginRight:5,
-        borderWidth: 2,
-        borderColor: '#000000',
-        margin: 0,
-        padding:2
-    },
+  card: {
+    marginLeft: 5,
+    marginRight: 5,
+    borderWidth: 2,
+    borderColor: '#000000',
+    margin: 0,
+    padding: 2
+  },
   image: {
     margin: 0,
     padding: 0,
     height: '100%'
   },
   row: {
-      marginTop:0,
-      marginBottom:0
+    marginTop: 0,
+    marginBottom: 0
   },
   text: {
     position: 'absolute',
     fontSize: 30,
     alignSelf: 'center',
-    marginTop: '3%'
+    marginTop: '3%',
+    backgroundColor: 'transparent'
   }
 });
