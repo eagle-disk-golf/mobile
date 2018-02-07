d3482c03f45343543f392ea8d36ed19db930ad77
Riku Laajala <riku.laajala@gmail.com>
2018-02-07 18:32:58 +0200

    score tweaks

--- src/components/summary-detail.js
+++ src/components/summary-detail.js
@@ -7,8 +7,8 @@
 */
 
 import React, {Component} from 'react';
-import {View, StyleSheet, Text, ScrollView, TouchableOpacity, } from 'react-native';
-import {Spinner} from 'native-base';
+import {View, StyleSheet,  ScrollView, TouchableOpacity} from 'react-native';
+import {Spinner, Text} from 'native-base';
 import {globalStyles} from '../res/styles';
 import {COLORS} from '../res/styles/constants';
 import firebase, {DB_NAMES} from '../services/firebase';
@@ -16,18 +16,31 @@ import {toArray} from '../helpers/data';
 
 const titleHeight = 60;
 const TitleComponent = ({item, index, testi}) => {
-  const score = item.throws.length - item.par;
+  const score = item.totalThrows - item.par;
   const scoreWithSign = score < 0 ? score.toString() : `+${score}`;
+  const scoreColor = score > 0 ? COLORS.danger : COLORS.success;
 
   return (
     <TouchableOpacity onPress={testi}>
-    <View style={[styles.titleContainer]}>
-    <View style={[styles.titleItemContainer, styles.borderRight]}><Text>{index + 1}</Text></View>
-    <View style={[styles.titleItemContainer, styles.borderRight]}><Text>{item.throws.length}</Text></View>
-    <View style={[styles.titleItemContainer, styles.borderRight]}><Text>{item.par}</Text></View>
-    <View style={styles.titleItemContainer}><Text>{score === 0 ? 'par' : scoreWithSign}</Text></View>
-  </View>
-  </TouchableOpacity>
+      <View style={[styles.titleContainer]}>
+        <View style={[styles.titleItemContainer, styles.borderRight]}>
+          <Text>{index + 1}</Text>
+        </View>
+
+        <View style={[styles.titleItemContainer, styles.borderRight]}>
+          <Text>{item.throws.length}</Text>
+          <Text style={styles.penalty}> {item.penalty ? `+${item.penalty}` : ''}</Text>
+        </View>
+
+        <View style={[styles.titleItemContainer, styles.borderRight]}>
+          <Text>{item.par}</Text>
+        </View>
+
+        <View style={styles.titleItemContainer}>
+          <Text style={{color: scoreColor}}>{score === 0 ? 'par' : scoreWithSign}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
   );
 };
 
@@ -45,12 +58,11 @@ export default class SummaryDetail extends Component {
     this.fetchData(course);
   }
 
-  showLoader() { this.setState({loading: true}); }
+  showLoader() {this.setState({loading: true});}
 
-  hideLoader() { this.setState({loading: false}); }
+  hideLoader() {this.setState({loading: false});}
 
   fetchData(course) {
-    console.log(course, 'course');
     this.showLoader();
 
     firebase.database().ref(DB_NAMES.lanes).orderByChild('courseId').equalTo(course.courseId).once('value').then(snapshot => {
@@ -66,27 +78,27 @@ export default class SummaryDetail extends Component {
     const {lanes, loading} = this.state;
     return (
       <View>
-           <ScrollView removeClippedSubviews={false} ref='scrollList' style={styles.container}>
-        <View renderToHardwareTextureAndroid={true} collapsable={false} ref='scrollListContainer' onLayout={() => {}}>
-          <View style={[globalStyles.bgSuccess, styles.titleContainer, styles.border]}>
-            <View style={[styles.titleItemContainer, styles.borderRight]}>
-              <Text style={globalStyles.textDefault}>Lane</Text>
+        <ScrollView removeClippedSubviews={false} ref='scrollList' style={styles.container}>
+          <View renderToHardwareTextureAndroid={true} collapsable={false} ref='scrollListContainer' onLayout={() => {}}>
+            <View style={[globalStyles.bgSuccess, styles.titleContainer, styles.border]}>
+              <View style={[styles.titleItemContainer, styles.borderRight]}>
+                <Text style={globalStyles.textDefault}>Lane</Text>
+              </View>
+              <View style={[styles.titleItemContainer, styles.borderRight]}>
+                <Text style={globalStyles.textDefault}>Throws</Text>
+              </View>
+              <View style={[styles.titleItemContainer, styles.borderRight]}>
+                <Text style={globalStyles.textDefault}>Par</Text>
+              </View>
+              <View style={styles.titleItemContainer}>
+                <Text style={globalStyles.textDefault}>Score</Text>
+              </View>
             </View>
-            <View style={[styles.titleItemContainer, styles.borderRight]}>
-              <Text style={globalStyles.textDefault}>Throws</Text>
-            </View>
-            <View style={[styles.titleItemContainer, styles.borderRight]}>
-              <Text style={globalStyles.textDefault}>Par</Text>
-            </View>
-            <View style={styles.titleItemContainer}>
-              <Text style={globalStyles.textDefault}>Score</Text>
-            </View>
-          </View>
 
-          {loading && <Spinner color="green" />}
-          {!loading && lanes && lanes.map((lane, index) => <TitleComponent key={index} index={index} item={lane} testi={() => this.props.navigation.navigate('SummaryDetailLane', {lane, index}) } /> )}
-        </View>
-      </ScrollView>
+            {loading && <Spinner color="green" />}
+            {!loading && lanes && lanes.map((lane, index) => <TitleComponent key={index} index={index} item={lane} testi={() => this.props.navigation.navigate('SummaryDetailLane', {lane, index})} />)}
+          </View>
+        </ScrollView>
       </View>
     );
   }
@@ -122,9 +134,16 @@ const styles = StyleSheet.create({
   titleItemContainer: {
     flex: 1,
     height: '100%',
+    // flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
     alignSelf: 'center',
+  },
+  penalty: {
+    position: 'absolute',
+    top: 21.5,
+    right: 15,
+    color: COLORS.danger
   },
   borderLeft: {
     borderLeftColor: 'black',
@@ -141,5 +160,5 @@ const styles = StyleSheet.create({
     width: '33%',
     paddingTop: 20,
     paddingBottom: 20
-  }
+  },
 });
