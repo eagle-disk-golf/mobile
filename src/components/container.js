0d4d9d0f642ab53baa9e713aacdf32f5da25b9d5
topinii <niiranentopi@gmail.com>
2018-02-19 12:19:39 +0200

    jsdoc

--- src/components/container.js
+++ src/components/container.js
@@ -4,13 +4,16 @@ import {globalStyles} from '../res/styles';
 import {stylesToArray} from '../helpers/components';
 
 export default class ContainerComponent extends Component {
+/**
+ * Represents a book.
+ * @constructor
+ * @param {string} title - The title of the book.
+ * @param {string} author - The author of the book.
+ */
   render() {
     const {style} = this.props;
     const styles = stylesToArray(style);
 
-    /**
-      * Returns a container with styles
-      */
     return (
       <Container style={[globalStyles.bgDefault, ...styles]}>{this.props.children}</Container>
     );
