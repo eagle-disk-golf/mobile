b1f356b834e04a99c9261751510143032d1c847d
Riku Laajala <riku.laajala@gmail.com>
2018-03-06 14:08:02 +0200

    use react-native-mauron85-background-geolocation

--- android/app/src/main/java/com/eaglediskgolf/MainApplication.java
+++ android/app/src/main/java/com/eaglediskgolf/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.agontuk.RNFusedLocation.RNFusedLocationPackage;
+import com.marianhello.react.BackgroundGeolocationPackage;  // <--- Import Package
 
 import java.util.Arrays;
 import java.util.List;
@@ -28,7 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
           new MainReactPackage(),
           new ReactNativeConfigPackage(),
           new MapsPackage(),
-          new RNFusedLocationPackage()
+          new RNFusedLocationPackage(),
+          new BackgroundGeolocationPackage()
       );
     }
 
--- src/components/gps-demo.js
+++ src/components/gps-demo.js
@@ -4,11 +4,117 @@ import {Button, Text} from 'native-base';
 import MapView, {PROVIDER_GOOGLE, Marker, Polyline, Polygon, Circle} from 'react-native-maps';
 import geolocation from '../services/geolocation';
 import {getDistanceInMetersBetweenCoordinates} from '../helpers/geolocation';
+import BackgroundGeolocation from 'react-native-mauron85-background-geolocation';
 
 const LATITUDE_DELTA = 0.01;
 const LONGITUDE_DELTA = 0.01;
 
 export default class GpsDemo extends Component {
+  componentDidMount() {
+    BackgroundGeolocation.configure({
+      desiredAccuracy: 5,
+      stationaryRadius: 1,
+      distanceFilter: 1,
+      notificationTitle: 'Background tracking',
+      notificationText: 'enabled',
+      debug: true,
+      startOnBoot: false,
+      stopOnTerminate: false,
+      locationProvider: BackgroundGeolocation.ACTIVITY_PROVIDER,
+      interval: 2000,
+      fastestInterval: 1000,
+      activitiesInterval: 2000,
+      stopOnStillActivity: false,
+      url: 'http://192.168.81.15:3000/location',
+      httpHeaders: {
+        'X-FOO': 'bar'
+      },
+      // customize post properties
+      postTemplate: {
+        lat: '@latitude',
+        lon: '@longitude',
+        foo: 'bar' // you can also add your own properties
+      }
+    });
+
+    BackgroundGeolocation.on('location', (location) => {
+      console.log(location, 'on location change');
+      const position = {
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+        ...location
+      };
+      this.setState({position, markers: [...this.state.markers, position], locationInitialized: true});
+
+      // handle your locations here
+      // to perform long running operation on iOS
+      // you need to create background task
+      BackgroundGeolocation.startTask(taskKey => {
+        // execute long running task
+        // eg. ajax post location
+        // IMPORTANT: task has to be ended by endTask
+        BackgroundGeolocation.endTask(taskKey);
+      });
+    });
+
+    BackgroundGeolocation.on('stationary', (stationaryLocation) => {
+      // handle stationary locations here
+      Actions.sendLocation(stationaryLocation);
+    });
+
+    BackgroundGeolocation.on('error', (error) => {
+      console.log('[ERROR] BackgroundGeolocation error:', error);
+    });
+
+    BackgroundGeolocation.on('start', () => {
+      console.log('[INFO] BackgroundGeolocation service has been started');
+    });
+
+    BackgroundGeolocation.on('stop', () => {
+      console.log('[INFO] BackgroundGeolocation service has been stopped');
+    });
+
+    BackgroundGeolocation.on('authorization', (status) => {
+      console.log('[INFO] BackgroundGeolocation authorization status: ' + status);
+      if (status !== BackgroundGeolocation.AUTHORIZED) {
+        Alert.alert('Location services are disabled', 'Would you like to open location settings?', [
+          {text: 'Yes', onPress: () => BackgroundGeolocation.showLocationSettings()},
+          {text: 'No', onPress: () => console.log('No Pressed'), style: 'cancel'}
+        ]);
+      }
+    });
+
+    BackgroundGeolocation.on('background', () => {
+      console.log('[INFO] App is in background');
+    });
+
+    BackgroundGeolocation.on('foreground', () => {
+      console.log('[INFO] App is in foreground');
+    });
+
+    BackgroundGeolocation.checkStatus(status => {
+      console.log('[INFO] BackgroundGeolocation service is running', status.isRunning);
+      console.log('[INFO] BackgroundGeolocation service has permissions', status.hasPermissions);
+      console.log('[INFO] BackgroundGeolocation auth status: ' + status.authorization);
+
+      // you don't need to check status before start (this is just the example)
+      if (!status.isRunning) {
+        BackgroundGeolocation.start(); //triggers start on start event
+        BackgroundGeolocation.getLocations((locations) => {
+            console.log(locations, 'locations');
+          }
+        );
+      }
+    });
+
+    // you can also just start without checking for status
+    // BackgroundGeolocation.start();
+  }
+
+  componentWillUnmount() {
+    // unregister all event listeners
+    BackgroundGeolocation.events.forEach(event => BackgroundGeolocation.removeAllListeners(event));
+  }
   constructor(props) {
     super(props);
     this.state = {
@@ -38,10 +144,6 @@ export default class GpsDemo extends Component {
           {text: 'Okay'},
         ]);
     });
-  }
-
-  componentDidMount() {
-    this.updatePosition();
   }
 
   render() {
